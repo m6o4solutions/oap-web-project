e52218d28324c8d4a7f7726603657966f05eb70b
Michael Owuor <michael.owuor@gmail.com>
2024-02-02 01:06:20 +0300

    refactor: home hero section

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,51 @@
+import { Metadata } from "next";
+import { getCldOgImageUrl } from "next-cloudinary";
+
+import { getHomePage } from "@/lib/data/read/index";
+
 import { HomeContentWidget, HomeHeroWidget } from "@/components/index";
 
+type HomePageProps = {
+	pageTitle: string;
+	pageMetaDescription: string;
+	heroActionBlock: {
+		content: {
+			content: { header: { title: string; subtitle: string } };
+			image: { public_id: string };
+		};
+	};
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+	const { pageTitle, pageMetaDescription, heroActionBlock }: HomePageProps =
+		await getHomePage();
+
+	return {
+		title: pageTitle,
+		description: pageMetaDescription,
+		openGraph: {
+			type: "article",
+			images: [
+				{
+					width: 1200,
+					height: 630,
+					url: getCldOgImageUrl({
+						src: heroActionBlock.content.image.public_id,
+						format: "jpg",
+					}),
+				},
+			],
+		},
+	};
+}
+
 const HomePage = async () => {
+	const { heroActionBlock }: HomePageProps = await getHomePage();
+
 	return (
 		<article className="mt-24 space-y-8">
 			<section id="hero">
-				<HomeHeroWidget />
+				<HomeHeroWidget heroActionBlock={heroActionBlock} />
 			</section>
 
 			<section id="content">
