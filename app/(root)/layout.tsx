e52a6a637e8a74dd219c2f0ca1036b9b567952b2
Michael Owuor <michael.owuor@gmail.com>
2024-12-04 19:41:04 +0300

    chore: update dependencies

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,10 +2,7 @@ import { ReactNode } from "react";
 
 import { getGlobal } from "@/lib/data/read/index";
 
-import {
-	NavigationFooterBlock,
-	NavigationHeaderBlock,
-} from "@/components/index";
+import { NavigationFooterBlock, NavigationHeaderBlock } from "@/components/index";
 
 type GlobalDataProps = {
 	siteName: string;
@@ -19,30 +16,18 @@ type LayoutProps = {
 };
 
 const Layout = async ({ children }: LayoutProps) => {
-	const {
-		siteName,
-		siteTagline,
-		navigationHeader,
-		navigationFooter,
-	}: GlobalDataProps = await getGlobal();
+	const { siteName, siteTagline, navigationHeader, navigationFooter }: GlobalDataProps = await getGlobal();
 
 	return (
 		<div className="flex h-screen flex-col">
 			<header>
-				<NavigationHeaderBlock
-					siteName={siteName}
-					navigationHeader={navigationHeader}
-				/>
+				<NavigationHeaderBlock siteName={siteName} navigationHeader={navigationHeader} />
 			</header>
 
 			<main>{children}</main>
 
 			<footer className="mt-auto">
-				<NavigationFooterBlock
-					siteName={siteName}
-					siteTagline={siteTagline}
-					navigationFooter={navigationFooter}
-				/>
+				<NavigationFooterBlock siteName={siteName} siteTagline={siteTagline} navigationFooter={navigationFooter} />
 			</footer>
 		</div>
 	);
